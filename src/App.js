e9d6f63f81f5276aad26650580e457b268b84e38
BABU BALAJI V <85435869+BABU-BALAJI-V@users.noreply.github.com>
2024-04-18 11:26:25 +0530

    commit
    logo and name

--- src/App.js
+++ src/App.js
@@ -7,10 +7,18 @@ import Skills from './components/Skills';
 import Resume from './components/Resume';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
+import {Helmet} from "react-helmet";
+
 
 function App() {
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Balaji Portfolio</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+        <meta name="description" content="Helmet application" />
+      </Helmet>
       <AppBar/>
       <HeroImg/>
       <About/>
