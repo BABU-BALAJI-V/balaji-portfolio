4d21f7457ae291c145884052ba386928cb3e506b
BABU BALAJI V <85435869+BABU-BALAJI-V@users.noreply.github.com>
2024-04-18 13:26:13 +0530

    commit
    small and medium sizing , sweetalert integration

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,11 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 import {Container} from 'react-bootstrap'
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Swal from 'sweetalert2';
 
 const Contact = () => {
     const form = useRef();
@@ -15,6 +20,11 @@ const Contact = () => {
             .then(
             () => {
                 console.log('SUCCESS!');
+                    Swal.fire({
+                    title: "Done!",
+                    text: "Email Sent!",
+                    icon: "success"
+                  });
             },
             (error) => {
                 console.log('FAILED...', error.text);
@@ -23,20 +33,36 @@ const Contact = () => {
         };
 
   return (
-    <Container id='contact' className='contact-main'>
+    <Container id='contact' className='form-div'>
             <h2 className='header-clr'>Contact</h2>
             <div className="divider"></div><br/>
             <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
 
-            <form ref={form} onSubmit={sendEmail}>
-                <label>Name</label>
-                <input type="text" name="user_name" />
-                <label>Email</label>
-                <input type="email" name="user_email" />
+            <Form ref={form} onSubmit={sendEmail}>
+                <Row className="mb-3">
+                    <Form.Group as={Col} controlId="formGridName">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="name" placeholder="Name" name="user_name"/>
+                    </Form.Group>
+
+                    <Form.Group as={Col} controlId="formGridEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" name="user_email"/>
+                    </Form.Group>                    
+                </Row>
+
+                <Form.Group className="mb-3" controlId="formGridPhoneno">
+                    <Form.Label>Phone.no</Form.Label>
+                    <Form.Control placeholder="Phone.no" name="user_ph_no"/>
+                </Form.Group>
+
                 <label>Message</label>
                 <textarea name="message" />
-                <input type="submit" value="Send" />
-            </form>
+
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
     </Container>
   )
 }
